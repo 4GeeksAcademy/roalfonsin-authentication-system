80c906149ea40a65a7ffb09f889554c2ab3c1f3c
RoAlfonsin <130806097+RoAlfonsin@users.noreply.github.com>
2023-08-10 18:13:43 +0000

    signup front created

---
+++ src/front/js/pages/signup.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+export function Signup() {
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(`Username: ${username}, Email: ${email}, Password: ${password}`);
+    };
+
+    return (
+        <div className="container">
+            <h2>Signup</h2>
+            <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label className="form-label"> Username: </label>
+                    <input type="text" className="form-control" value={username} onChange={(e) => setUsername(e.target.value)} />
+                </div>
+                
+                <div className="mb-3">
+                    <label className="form-label"> Email: </label>
+                    <input type="email" className="form-control" value={email} onChange={(e) => setEmail(e.target.value)} />
+                </div>
+
+                <div className="mb-3">
+                    <label className="form-label"> Password: </label>
+                    <input type="password" className="form-control" value={password} onChange={(e) => setPassword(e.target.value)} />
+                </div>
+
+                <button type="submit" className="btn btn-success">Submit</button>
+            </form>
+        </div>
+    );
+}
