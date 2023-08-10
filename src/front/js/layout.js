fc7f7dc2fa342d0243f1071bba128a1591ed1fb3
RoAlfonsin <130806097+RoAlfonsin@users.noreply.github.com>
2023-08-10 18:47:39 +0000

    login and private front created

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -5,6 +5,8 @@ import { BackendURL } from "./component/backendURL";
 
 import { Home } from "./pages/home";
 import { Signup } from "./pages/signup.jsx";
+import { Login } from "./pages/login.jsx";
+import {PrivatePage} from "./pages/private.jsx";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -26,8 +28,8 @@ const Layout = () => {
                     <Routes>
                         <Route element={<Home />} path="/" />
                         <Route element={<Signup />} path="/signup"/>
-                        <Route element={<h1>This is the login</h1>} path="/login"/>
-                        <Route element={<h1>This is the private</h1>} path="/private"/>
+                        <Route element={<Login />} path="/login"/>
+                        <Route element={<PrivatePage />} path="/private"/>
                         <Route element={<h1>Not found!</h1>} path="*"/>
                     </Routes>
                     <Footer />
---
+++ src/front/js/pages/private.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function PrivatePage(){
+    return (
+        <div className="container">
+            <h1>Congrats, this is the private page</h1>
+            <h2>You are the best member</h2>
+        </div>
+    )
+}
+
